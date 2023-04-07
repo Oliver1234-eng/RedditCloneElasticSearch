d36123c814a838fa2fd72438aec454f63ac0850b
Oliver1234-eng <oliver.kojic22@gmail.com>
2023-04-07 17:23:12 +0200

    Uradjena je inicijalizacija Spring Boot projekta

---
+++ redditcloneprojekat/src/main/java/com/ftn/redditcloneprojekat/RedditcloneprojekatApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.redditcloneprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditcloneprojekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditcloneprojekatApplication.class, args);
+	}
+
+}
